2a148b449a5765d1f954d2d63b29101ed7a302c4
DaveOkpare <okparedave@gmail.com>
2023-01-22 17:59:57 +0100

    Refactor tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,16 +4,26 @@ from unittest.mock import Mock
 from wagwan.utils import *
 
 
-@mock.patch("os.path.isfile")
-def test_path_exists(mock_os_path_isfile):
-    mock_os_path_isfile.return_value = Mock(output=True)
+@mock.patch("os.path.isfile", Mock(return_value=True))
+def test_path_exists():
     response = path_exists()
-    assert response.output is True
+    assert response is True
 
 
-@mock.patch("typer.prompt")
-def test_get_key(mock_typer_prompt):
-    mock_typer_prompt.return_value = Mock(key="API_KEY")
+@mock.patch("typer.prompt", Mock(return_value="API_KEY"))
+def test_get_key():
     response = get_key()
-    assert response.key is not None
-    assert response.key == "API_KEY"
+    assert response is not None
+    assert response == "API_KEY"
+
+
+# @mock.patch()
+# @mock.patch("typing.IO.readlines", Mock(return_value="OUTPUT_STRING"))
+# def test_read_file():
+#     response = OpenAI(file_path="~/temp").read_file()
+#     assert response is not None
+
+@mock.patch("wagwan.utils.OpenAI.read_file", Mock(return_value="This is output text"))
+def test_explain_prompt():
+    response = OpenAI(file_path="~/temp").explain_prompt()
+    assert response is not None
